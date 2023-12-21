5fb9f2128f4f0a497c08089af2ae3a9c042bc880
cttlee <cttlee@msn.com>
2023-12-21 16:17:25 +0800

    Metadata: Use the local taken time for location estimates photoprism#3958

--- internal/entity/photo_estimate.go
+++ internal/entity/photo_estimate.go
@@ -99,15 +99,15 @@ func (m *Photo) EstimateLocation(force bool) {
 		err = UnscopedDb().
 			Where("photo_lat <> 0 AND photo_lng <> 0").
 			Where("place_src <> '' AND place_src <> ? AND place_id IS NOT NULL AND place_id <> '' AND place_id <> 'zz'", SrcEstimate).
-			Where("taken_src <> '' AND taken_at BETWEEN CAST(? AS DATETIME) AND CAST(? AS DATETIME)", rangeMin, rangeMax).
-			Order(gorm.Expr("ABS(TIMESTAMPDIFF(SECOND, taken_at, ?))", m.TakenAt)).Limit(2).
+			Where("taken_at_local <> '' AND taken_at_local BETWEEN CAST(? AS DATETIME) AND CAST(? AS DATETIME)", rangeMin, rangeMax).
+			Order(gorm.Expr("ABS(TIMESTAMPDIFF(SECOND, taken_at_local, ?))", m.TakenAt)).Limit(2).
 			Preload("Place").Find(&mostRecent).Error
 	case SQLite3:
 		err = UnscopedDb().
 			Where("photo_lat <> 0 AND photo_lng <> 0").
 			Where("place_src <> '' AND place_src <> ? AND place_id IS NOT NULL AND place_id <> '' AND place_id <> 'zz'", SrcEstimate).
-			Where("taken_src <> '' AND taken_at BETWEEN ? AND ?", rangeMin, rangeMax).
-			Order(gorm.Expr("ABS(JulianDay(taken_at) - JulianDay(?))", m.TakenAt)).Limit(2).
+			Where("taken_at_local <> '' AND taken_at_local BETWEEN ? AND ?", rangeMin, rangeMax).
+			Order(gorm.Expr("ABS(JulianDay(taken_at_local) - JulianDay(?))", m.TakenAt)).Limit(2).
 			Preload("Place").Find(&mostRecent).Error
 	default:
 		log.Warnf("photo: unsupported sql dialect %s", clean.Log(DbDialect()))
